ba4e3c5eb6598a1d622fe250535514df33684092
Jason Kent <jasonbkent@gmail.com>
2020-09-18 13:22:52 -0600

    Table of Contents

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 function listStuff() {
-    document.getElementsByName("ol")
+    var list = document.getElementById('list');
+    var arrayList = ['./week1/form.html']
+    var arrayName = ['Week 1']
+
+    for (let i = 0; i < arrayList.length; i++) {
+        const element = arrayList[i];
+        const name = arrayName[i];
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+
+        a.setAttribute('href', element);
+
+        a.innerText = name;
+
+        li.appendChild(a);
+
+        list.appendChild(li);
+    }
 }
