a97d008e53ccebb2ddd7b20eb92a295a80ce4f39
Jason Kent <jasonbkent@gmail.com>
2020-11-07 14:41:26 -0700

    Updated ToC

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function listStuff() {
     var list = document.getElementById('list');
-    var arrayList = ['./Notes/W01story.html', './Notes/W02Notes.html', './Notes/W03Notes.html', './Notes/W04Notes.html', './Notes/W05Notes.html', './ToDo/todo.html', './Notes/W07Notes.html'];
-    var arrayName = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'To Do Project', 'Week 7'];
+    var arrayList = ['./Notes/W01story.html', './Notes/W02Notes.html', './Notes/W03Notes.html', './Notes/W04Notes.html', './Notes/W05Notes.html', './ToDo/todo.html', './Notes/W07Notes.html', './Notes/W08Notes.html'];
+    var arrayName = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'To Do Project', 'Week 7', 'Week 08'];
 
     for (let i = 0; i < arrayList.length; i++) {
         const element = arrayList[i];
